be68fbd13998e35e83c44480223776f868906b69
penadigital00 <167214031+penadigital00@users.noreply.github.com>
2026-04-19 03:10:46 +0700

    videocut 0.2.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/videocut.rb
+++ Formula/videocut.rb
@@ -3,8 +3,8 @@ class Videocut < Formula
 
   desc "AI-powered video automation tool for Content Creators"
   homepage "https://github.com/pendig/videocut-cli"
-  url "https://github.com/pendig/videocut-cli/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "177b3f04e25a0fdbdf6154254ef801d82301a6ec0d343917e41a6f6eba18c918"
+  url "https://github.com/pendig/videocut-cli/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "05c9894f056d961c49ec70200ebc49c6b81543b24cba92870fad87c5d778cdc5"
   license "MIT"
 
   depends_on "ffmpeg"
